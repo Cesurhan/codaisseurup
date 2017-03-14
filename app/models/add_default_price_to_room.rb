7e0cd63b0c25e847d42a1d6174876beb2f2b0ad8
Cesurhan <ces-ur@hotmail.com>
2017-03-14 13:36:51 +0100

    One-to-Many: Controllers

---
+++ app/models/add_default_price_to_room.rb
@@ -0,0 +1,2 @@
+class AddDefaultPriceToRoom < ApplicationRecord
+end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,9 @@
 class Event < ApplicationRecord
   belongs_to :user
+
   validates :name, presence: true
-  validates :description, presence: true, length {maximum: 500}
+  validates :description, presence: true, length: { maximum: 500 }
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+  validates if :starts_at >= :ends_at
 end
