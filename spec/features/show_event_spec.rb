ffa311026b4df333466ee200366275f3c98930e5
Cesurhan <ces-ur@hotmail.com>
2017-03-15 15:04:09 +0100

    Add test for event name

--- spec/features/show_event_spec.rb
+++ spec/features/show_event_spec.rb
@@ -1,15 +1,14 @@
 require 'rails_helper'
 
-describe "Viewing an individual event" do
+describe 'Viewing an individual event' do
   let(:event) { create :event }
-  # let(:user) { create :user}
 
   it "shows the event's details" do
     visit event_url(event)
 
+    expect(page).to have_text(event.name)
     expect(page).to have_text(event.description)
     expect(page).to have_text(event.location)
-    # expect(page).to have_text(user.email)
     expect(page).to have_text(event.price)
     expect(page).to have_text(event.capacity)
   end
