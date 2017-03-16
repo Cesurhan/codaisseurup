3e0dda60ac2c1a5ad1a203efab4bef37f0b9f1f4
Cesurhan <ces-ur@hotmail.com>
2017-03-16 15:47:37 +0100

    Create Photo: Controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,7 +7,8 @@ class EventsController < ApplicationController
   end
 
   def show
-      @categories = @event.categories
+    @categories = @event.categories
+    @photos = @event.photos
   end
 
   def new
@@ -17,27 +18,38 @@ class EventsController < ApplicationController
   def create
     @event = current_user.events.build(event_params)
 
-  if @event.save
-    redirect_to @event, notice: "Room successfully created"
-  else
-    render :new
+    if @event.save
+      image_params.each do |image|
+        @event.photos.create(image: image)
+      end
+
+      redirect_to edit_event_path, notice: 'Room successfully created'
+    else
+      render :new
+    end
   end
-end
 
-def edit
-end
-
-def update
-  if @event.update(event_params)
-    redirect_to @event, notice: "Event successfully updated"
-  else
-    render :edit
+  def edit
+    if current_user.id == @event.user.id
+      @photos = @event.photos
+    else
+      redirect_to root_path notice: "You don't have permission"
+    end
   end
-end
 
+  def update
+    if @event.update(event_params)
+      image_params.each do |image|
+        @event.photos.create(image: image)
+      end
 
+      redirect_to edit_event_path(@event), notice: 'Event successfully updated'
+    else
+      render :edit
+    end
+  end
 
-private
+  private
 
   def set_event
     @event = Event.find(params[:id])
@@ -47,4 +59,7 @@ private
     params.require(:event).permit(:name, :description, :location, :price, :capacity, :includes_food, :includes_drinks, :starts_at, :ends_at, :active, category_ids: [])
   end
 
+  def image_params
+    params[:images].present? ? params.require(:images) : []
+  end
 end
